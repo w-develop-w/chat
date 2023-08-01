ef21c1179eef05622129a4a4892127261e3d1f93
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-08-01 08:19:08 +0300

    (5)just save

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,73 +1,204 @@
-import React, { useEffect } from "react";
-import ScrollToBottom from "react-scroll-to-bottom";
-import { setCurrentMessage, setMessageList } from "./store/dataSlice";
-import { useAppDispatch, useAppSelector } from "./hooks";
+// import React, { useEffect } from "react"
+// import ScrollToBottom from "react-scroll-to-bottom"
+// import { setCurrentMessage, setMessageList } from "./store/dataSlice"
+// import { useAppDispatch, useAppSelector } from "./hooks"
+
+// function Chat({ socket, userName, room }) {
+//     const currentMessage = useAppSelector(
+//         (state) => state.dataOfChat.currentMessage
+//     )
+//     const messageList = useAppSelector((state) => state.dataOfChat.initialstate.messageList)
+//     const dispatch = useAppDispatch()
+
+//     //   const sendMessage = async () => {
+//     //     if (currentMessage !== "") {
+//     //       const messageData = {
+//     //         room: room,
+//     //         userName: userName,
+//     //         message: currentMessage,
+//     //         time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
+//     //       };
+
+//     //       await socket.emit("send_message", messageData);
+//     //       dispatch(setMessageList((list) => [...list, messageData]));
+//     //     }
+//     //   };
+
+//     const sendMessage = async () => {
+//         if (currentMessage !== "") {
+//             const messageData = {
+//                 room: room,
+//                 userName: userName,
+//                 message: currentMessage,
+//                 time:
+//                     new Date(Date.now()).getHours() +
+//                     ":" +
+//                     new Date(Date.now()).getMinutes(),
+//             }
+
+//             await socket.emit("send_message", messageData)
+
+//             // Instead of passing a function directly as payload, create a new array
+//             const updatedMessageList = [...messageList, messageData]
+
+//             dispatch(setMessageList(updatedMessageList))
+//         }
+//     }
+
+//     useEffect(() => {
+//         socket.on("receive_message", (data) => {
+//             dispatch(setMessageList((list) => [...list, data]))
+//         })
+//     }, [socket, dispatch]) // Include 'dispatch' in the dependency array
+
+//     const handleKeyPress = (event) => {
+//         event.key === "Enter" && sendMessage()
+//     }
+//     return (
+//         <div className="chat-window">
+//             <div className="chat-header">
+//                 <p>Live Chat</p>
+//             </div>
+
+//             <div className="chat-body">
+//                 <ScrollToBottom className="message-container">
+//                     {Array.isArray(messageList) &&
+//                         messageList.map((messageContent, index) => (
+//                             <div
+//                                 key={index}
+//                                 className="message"
+//                                 id={
+//                                     userName === messageContent.userName
+//                                         ? "you"
+//                                         : "other"
+//                                 }
+//                             >
+//                                 <div>
+//                                     <div className="message-content">
+//                                         <p>{messageContent.message}</p>
+//                                     </div>
+
+//                                     <div className="message-meta">
+//                                         <p id="time">{messageContent.time}</p>
+//                                         <p id="author">
+//                                             {messageContent.userName}
+//                                         </p>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         ))}
+//                 </ScrollToBottom>
+//             </div>
+//             <div className="chat-footer">
+//                 <input
+//                     type="text"
+//                     value={currentMessage}
+//                     placeholder="Input your message"
+//                     onChange={(event) =>
+//                         dispatch(setCurrentMessage(event.target.value))
+//                     }
+//                     onKeyDown={handleKeyPress}
+//                 />
+//                 <button onClick={sendMessage}>&#9658;</button>
+//             </div>
+//         </div>
+//     )
+// }
+// export default Chat
+
+
+
+import React, { useEffect } from "react"
+import ScrollToBottom from "react-scroll-to-bottom"
+import { setCurrentMessage, setMessageList } from "./store/dataSlice"
+import { useAppDispatch, useAppSelector } from "./hooks"
 
 function Chat({ socket, userName, room }) {
-  const currentMessage = useAppSelector((state) => state.dataOfChat.currentMessage);
-  const messageList = useAppSelector((state) => state.dataOfChat.messageList);
-  const dispatch = useAppDispatch();
+    const currentMessage = useAppSelector(
+        (state) => state.dataOfChat.currentMessage
+    )
+    const messageList = useAppSelector((state) => state.dataOfChat.messageList);
 
-  const sendMessage = async () => {
-    if (currentMessage !== "") {
-      const messageData = {
-        room: room,
-        userName: userName,
-        message: currentMessage,
-        time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
-      };
+    // const messageList = useAppSelector((state) => state.dataOfChat.messageList)
+    const dispatch = useAppDispatch()
 
-      await socket.emit("send_message", messageData);
-      dispatch(setMessageList((list) => [...list, messageData]));
+    const sendMessage = async () => {
+        if (currentMessage !== "") {
+            const messageData = {
+                room: room,
+                userName: userName,
+                message: currentMessage,
+                time:
+                    new Date(Date.now()).getHours() +
+                    ":" +
+                    new Date(Date.now()).getMinutes(),
+            }
+
+            await socket.emit("send_message", messageData)
+
+            const updatedMessageList = [...messageList, messageData]
+
+            dispatch(setMessageList(updatedMessageList))
+        }
     }
-  };
 
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      dispatch(setMessageList((list) => [...list, data]));
-    });
-  }, [socket, dispatch]); // Include 'dispatch' in the dependency array
+    useEffect(() => {
+        socket.on("receive_message", (data) => {
+            dispatch(setMessageList((list) => [...list, data]))
+        })
+    }, [socket, dispatch]) // Include 'dispatch' in the dependency array
 
-  const handleKeyPress = (event) => {
-    event.key === "Enter" && sendMessage();
-  };
-
-  return (
-    <div className="chat-window">
-      <div className="chat-header">
-        <p>Live Chat</p>
-      </div>
-
-      <div className="chat-body">
-        <ScrollToBottom className="message-container">
-          {messageList && messageList.map((messageContent, index) => (
-            <div key={index} className="message" id={userName === messageContent.userName ? "you" : "other"}>
-              <div>
-                <div className="message-content">
-                  <p>{messageContent.message}</p>
-                </div>
-
-                <div className="message-meta">
-                  <p id="time">{messageContent.time}</p>
-                  <p id="author">{messageContent.userName}</p>
-                </div>
-              </div>
+    const handleKeyPress = (event) => {
+        event.key === "Enter" && sendMessage()
+    }
+    return (
+        <div className="chat-window">
+            <div className="chat-header">
+                <p>Live Chat</p>
             </div>
-          ))}
-        </ScrollToBottom>
-      </div>
-      <div className="chat-footer">
-        <input
-          type="text"
-          value={currentMessage}
-          placeholder="Input your message"
-          onChange={(event) => dispatch(setCurrentMessage(event.target.value))}
-          onKeyDown={handleKeyPress}
-        />
-        <button onClick={sendMessage}>&#9658;</button>
-      </div>
-    </div>
-  );
-}
 
-export default Chat;
+            <div className="chat-body">
+                <ScrollToBottom className="message-container">
+                    { messageList.length > 0 &&
+                        messageList.map((messageContent, index) => (
+                            <div
+                                key={index}
+                                className="message"
+                                id={
+                                    userName === messageContent.userName
+                                        ? "you"
+                                        : "other"
+                                }
+                            >
+                                <div>
+                                    <div className="message-content">
+                                        <p>{messageContent.message}</p>
+                                    </div>
+
+                                    <div className="message-meta">
+                                        <p id="time">{messageContent.time}</p>
+                                        <p id="author">
+                                            {messageContent.userName}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                </ScrollToBottom>
+            </div>
+            <div className="chat-footer">
+                <input
+                    type="text"
+                    value={currentMessage}
+                    placeholder="Input your message"
+                    onChange={(event) =>
+                        dispatch(setCurrentMessage(event.target.value))
+                    }
+                    onKeyDown={handleKeyPress}
+                />
+                <button onClick={sendMessage}>&#9658;</button>
+            </div>
+        </div>
+    )
+}
+export default Chat
--- client/src/store/dataSlice.ts
+++ client/src/store/dataSlice.ts
@@ -1,16 +1,45 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
-type MessageList = [
+// type MessageList = [
+//     room: string,
+//     userName: string,
+//     message: string,
+//     time: string
+// ]
+
+// type MessageList = {
+//     room: string;
+//     userName: string;
+//     message: string;
+//     time: string;
+//   }[];
+  
+
+// type RootState = {
+//     currentMessage: string,
+//     messageList: String[]
+// }
+
+// type MessageData = [
+//     room: string,
+//     userName: string,
+//     message: string,
+//     time: string,
+// ];
+type MessageData = [
     room: string,
     userName: string,
     message: string,
     time: string
-]
+];
+  
+  type RootState = {
+    currentMessage: string;
+    messageList: MessageData[];
+  };
+  
+  
 
-type RootState = {
-    currentMessage: string,
-    messageList: MessageList[]
-}
 
 const initialState: RootState = {
     currentMessage: "",
@@ -21,14 +50,14 @@ const dataOfChatSlice = createSlice({
     name: "dataOfChat",
     initialState,
     reducers: {
-        setCurrentMessage: (state, action: PayloadAction<string>) => {
-            state.currentMessage = action.payload
-        },
-        setMessageList: (state, action: PayloadAction<MessageList[]>) => {
-            state.messageList = action.payload
-        }
+      setCurrentMessage: (state, action: PayloadAction<string>) => {
+        state.currentMessage = action.payload;
+      },
+      setMessageList: (state, action: PayloadAction<MessageData[]>) => {
+        state.messageList = action.payload;
+      },
     },
-})
-
+  });
+  
 export const { setCurrentMessage, setMessageList } = dataOfChatSlice.actions
 export default dataOfChatSlice.reducer
